53dba3e995f8bfd6645f5cb0b5a8e867a8a162cc
Aditya <adityakumarsingh@gmail.com>
2022-10-13 10:14:41 +0530

    song added

--- src/Story/index.js
+++ src/Story/index.js
@@ -1,7 +1,8 @@
 import useAnimation from "./useAnimation";
 import "./styles.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import us from './us2.png';
+import song from './suno.mp3'
 
 import kahani from "./kahani";
 
@@ -18,11 +19,38 @@ const colors = [
 	'#ffcaff',
 ]
 
+var audio = new Audio(song);
+
 export default function Story() {
   const [current, setCurrent] = useState(0);
+	const [toggleMusic, setToggleMusic] = useState(false);
+	const [isDesktop, setIsDesktop] = useState(false);
   const { mainPlay, mainStyle, nextPlay, nextStyle, prevPlay, prevStyle, imagePlay, imageStyle } =
     useAnimation(setCurrent);
 
+	useEffect(() => {
+		if (toggleMusic) {
+			audio.play();
+		} else
+			audio.pause();
+	}, [toggleMusic])
+
+	useEffect(() => {
+		var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+    if (mobile) {
+			setIsDesktop(false);
+    } else {
+			setIsDesktop(true);
+    }
+	}, [])
+
+	if (!isDesktop) {
+		return (
+			<div className="mobile">
+				<h1>Sorry, this story is not available on mobile devices.</h1>
+			</div>
+		)
+	}
   return (
     <div className="Story" style={{
 			backgroundColor: colors[current - 26] || colors[0],
@@ -49,6 +77,7 @@ export default function Story() {
             setCurrent(current + 1);
 						if (current === (kahani.length - 1)) {
 							imagePlay(true);
+							setToggleMusic(true);
 						}
           }}
         >
@@ -61,7 +90,7 @@ export default function Story() {
       </div>
       )}
 			{!kahani[current] && (
-				<div className="card" style={imageStyle}>
+				<div className="card" style={imageStyle} onClick={() => setToggleMusic(o => !o)}>
 					<img src={us} className="fade-in-image" alt="our_story" />
 					<p>Dont worry, will remove this site today.</p>
 			</div>
